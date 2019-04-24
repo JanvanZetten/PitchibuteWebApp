8cfba8f0887f99d93be5dd48361a75995cc9b3ae
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2019-04-24 15:04:02 +0200

    Made check for if passwords match

--- src/app/start/registrer/registrer.component.ts
+++ src/app/start/registrer/registrer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-registrer',
@@ -15,17 +14,24 @@ export class RegistrerComponent implements OnInit {
     comfirmPassword: new FormControl()
   })
 
+  showPasswordNotMatchError = false;
+
   constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
 
+  checkPasswordMatch() {
+    this.showPasswordNotMatchError = this.registrerForm.value.password !== this.registrerForm.value.comfirmPassword
+  }
+
   registrer() {
+    // TODO Add some requirements for the passwords like min. length.
+    this.checkPasswordMatch()
     if (this.registrerForm.value.password === this.registrerForm.value.comfirmPassword) {
       let promiseUserCredential = this.afAuth.auth.createUserWithEmailAndPassword(this.registrerForm.value.email, this.registrerForm.value.password)
+
       // TODO Act on what the promise returns.
     }
-    // TODO If the passwords where not equal then tell the user.
-    // TODO Add some requirements for the passwords like min. length.
   }
 }
