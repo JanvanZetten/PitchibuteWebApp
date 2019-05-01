fb22a91202909ab964b4dc831ca76e8b318bd5bd
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2019-05-01 11:37:26 +0200

    Fixed a problem with bad import naming and added path metadata to the upload function, also made the initial test work

--- src/app/shared/file-upload/file-upload-service/file-upload.service.spec.ts
+++ src/app/shared/file-upload/file-upload-service/file-upload.service.spec.ts
@@ -1,12 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 
 import { FileUploadService } from './file-upload.service';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { CommonModule } from '@angular/common';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
 
 describe('FileUploadService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  /*it('should be created', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [FileUploadService],
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        AngularFireStorageModule
+      ]
+    });
+  }));
+
+  it('should be created', () => {
     const service: FileUploadService = TestBed.get(FileUploadService);
     expect(service).toBeTruthy();
-  });*/
+  });
+
+
 });
