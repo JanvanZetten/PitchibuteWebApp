04f0d606e406331d9758d969678eb4913876fdb7
Husso1997 <hussain1961@hotmail.com>
2019-05-06 11:19:19 +0200

    Removed unused imports

--- src/app/start/login/login.component.spec.ts
+++ src/app/start/login/login.component.spec.ts
@@ -1,16 +1,10 @@
-import {auth} from 'firebase/app';
 import {ReactiveFormsModule} from '@angular/forms';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {LoginComponent} from './login.component';
-import {$} from 'protractor';
 import {By} from '@angular/platform-browser';
-import {Input} from '@angular/core';
 import {RouterTestingModule} from '@angular/router/testing';
 import {RegistrerComponent} from '../registrer/registrer.component';
-import {AngularFireAuthModule, AngularFireAuth} from '@angular/fire/auth';
-import {AngularFireModule} from '@angular/fire';
-import {environment} from 'src/environments/environment';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '../../shared/authentication/authentication-service/authentication.service';
 
