4c318a61a67b5a8bcf7ec37bacb0f10eaa1a4753
JanvanZetten <janx3779@easv365.dk>
2019-05-06 15:05:37 +0200

    Feature/file upload with file structure (#7)
    * Fixed a problem with bad import naming and added path metadata to the upload function, also made the initial test work
    
    * Made a test, but it makes the other test fail this should be fixed
    
    * Added font but it has nothing to do with this branch
    
    * Made a functioning test to check correct file structure meta data for the function to make the structure
    
    * all the other files are saved to files not file so i think it should be files

--- src/app/shared/file-upload/file-upload-service/file-upload.service.spec.ts
+++ src/app/shared/file-upload/file-upload-service/file-upload.service.spec.ts
@@ -1,12 +1,70 @@
-import { TestBed } from '@angular/core/testing';
+import { Item, type } from './../../../entities/item';
+import { TestBed, async } from '@angular/core/testing';
 
 import { FileUploadService } from './file-upload.service';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorageModule, AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { first } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 
 describe('FileUploadService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  /*it('should be created', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [FileUploadService],
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        AngularFireStorageModule
+      ]
+    });
+  }));
+
+  it('should be created', () => {
     const service: FileUploadService = TestBed.get(FileUploadService);
     expect(service).toBeTruthy();
-  });*/
+  });
+
+  it('should make path with the item ids', () => {
+    const itemArray: Item[] = [
+      { id: 'outermostId', name: 'someName', type: type.group },
+      { id: 'secondId', name: 'someName', type: type.event },
+      { id: 'secondToLastId', name: 'someName', type: type.folder },
+      { id: 'lastId', name: 'someName', type: type.folder },
+    ]
+
+    const fileName = 'theFilename'
+    const uniqueId = "SomeVeryUniqueID"
+    const exspectetPathOutput = itemArray[0].id + '/' + itemArray[1].id + '/' + itemArray[2].id + '/' + itemArray[3].id + '/'
+    const service: FileUploadService = TestBed.get(FileUploadService);
+    const theFile = { name: fileName, size: 1, slice: null, lastModified: null, type: null }
+
+    const fireStorage = TestBed.get(AngularFireStorage)
+    const firestore = TestBed.get(AngularFirestore)
+    const fireStorageRefrence: AngularFireStorageReference =
+    {
+      getDownloadURL: null,
+      getMetadata: null,
+      delete: null,
+      child: null,
+      updateMetatdata: null,
+      put: (data, meteData) => { return null },
+      putString: null
+    }
+
+    spyOn(firestore, 'createId').and.returnValue(uniqueId)
+    spyOn(fireStorage, 'ref').and.returnValue(fireStorageRefrence)
+    spyOn(fireStorageRefrence, 'put').and.returnValue(of({}).toPromise())
+
+    const meta = { originalName: fileName, path: exspectetPathOutput }
+
+    const observable = service.upload(itemArray, theFile).pipe(first()).subscribe(() => {
+      expect(fireStorageRefrence.put).toHaveBeenCalledWith(theFile, { customMetadata: meta })
+      observable.unsubscribe()
+    })
+  })
 });
