8cb6a0de48cd3f64b7d803d1ff6ae337c52e8c09
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2019-05-07 10:45:26 +0200

    Added autofill properties to the input fields to get better autofill

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { AuthGuardService } from './auth-guard.service';
 import { WelcomeComponent } from './start/welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
