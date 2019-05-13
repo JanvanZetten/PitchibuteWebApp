fea21d96fbe0543bc2ad3ae7572a0b884677abd9
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2019-05-13 12:09:27 +0200

    Download by functions

--- src/app/download/download.module.ts
+++ src/app/download/download.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { DownloadRoutingModule } from './download-routing.module';
 import { DownloadfileComponent } from './downloadfile/downloadfile.component';
-import {FileDownloadServiceModule} from '../shared/file-download/file-download-service.module';
+import { FileDownloadServiceModule } from '../shared/file-download/file-download-service.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [DownloadfileComponent],
   imports: [
     CommonModule,
     DownloadRoutingModule,
-    FileDownloadServiceModule
+    FileDownloadServiceModule,
+    HttpClientModule,
   ],
 })
 export class DownloadModule { }
--- src/app/shared/file-download/file-download-service/file-download.service.ts
+++ src/app/shared/file-download/file-download-service/file-download.service.ts
@@ -2,16 +2,48 @@ import { Injectable } from '@angular/core';
 import {Group} from '../../../entities/group';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {FileDownloadServiceModule} from '../file-download-service.module';
-import {Observable} from 'rxjs';
+import { Observable, ReplaySubject, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { map, switchMap } from 'rxjs/operators';
+import { type as Type } from '../../../entities/item';
 
 @Injectable({
   providedIn: FileDownloadServiceModule
 })
 export class FileDownloadService {
 
-  constructor(private fireStorage: AngularFireStorage) { }
+  constructor(private fireStorage: AngularFireStorage,
+              private http: HttpClient) { }
 
-  downloadFile(fileId: string, group: Group): Observable<any> {
-    return this.fireStorage.ref(group.name + fileId).getDownloadURL();
+  /*
+   * Using a function to download file.
+   */
+  downloadFileFromFunctions(path: string): Observable<{ fileName: string, blob: Blob }> {
+    // Create url
+    const url = 'https://us-central1-pitchibute.cloudfunctions.net/downloadfile';
+
+    // set content and content type.
+    const headers = new HttpHeaders().set("Content-Type", "application/json");
+    const body = { path: path };
+
+    // Make HTTP Request.
+    return this.http.post(url, body, {
+      headers: headers,
+      responseType: 'blob' as 'json',
+      observe: 'response'
+    }).pipe(map((response: HttpResponse<any>) => {
+      // Get Content-Disposition Header.
+      var contentDispositionHeader = response.headers.get('Content-Disposition');
+
+      // Get filename from header.
+      var fileName = contentDispositionHeader.split(';')[1].trim().split('=')[1];
+      // Remove " from filename.
+      fileName = fileName.replace(/"/g, '');
+
+      // Return filename and blob.
+      return ({ fileName: fileName, blob: response.body });
+      }, err => {
+        throwError(err);
+    }));
   }
 }
