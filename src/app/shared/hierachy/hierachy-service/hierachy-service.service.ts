793ad8526aa22a8174ae511f22bf281f45cfa72f
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2019-05-15 12:36:05 +0200

    fixed the itemsURI error

--- src/app/shared/hierachy/hierachy-service/hierachy-service.service.ts
+++ src/app/shared/hierachy/hierachy-service/hierachy-service.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { HierachyServiceModule } from './hierachy-service.module';
 import { Item } from 'src/app/entities/item';
 
@@ -14,23 +12,9 @@ export class HierachyServiceService {
 
   uid: string;
 
-  constructor(private db: AngularFirestore,
-    private http: HttpClient,
-    private auth: AngularFireAuth) { }
+  constructor(private http: HttpClient) { }
 
   getChildItemsFromFirebaseFunction(path: Item[]): Observable<Item[]> {
-    /* Some stuff that is not used but it was here, TODO ask Alex about this
-    const self = this;
-    // This cannot get the UID needed in the header below.
-    this.auth.auth.onAuthStateChanged(function (user) {
-      if (user) {
-        self.uid = user.uid;
-      } else {
-        console.log('No user is logged in 1');
-      }
-    });
-    */
-
     const headers = {
       headers: new HttpHeaders({
         'uid': 'keyString',
@@ -45,7 +29,7 @@ export class HierachyServiceService {
     const itemsUriPart = '/items'
     let currentPathString = itemsUriPart;
     path.forEach(arrayItem => {
-      currentPathString = currentPathString + '/' + arrayItem.id + '/items';
+      currentPathString = currentPathString + '/' + arrayItem.id + itemsUriPart;
     });
     return currentPathString;
   }
