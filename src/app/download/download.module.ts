7523394f808f0a320b5e495c53e91fba8aebcd2c
asbamse <asbj0378@easv365.dk>
2019-05-23 09:11:43 +0200

    Integrated download button (#33)

--- src/app/download/download.module.ts
+++ src/app/download/download.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { DownloadRoutingModule } from './download-routing.module';
 import { DownloadfileComponent } from './downloadfile/downloadfile.component';
 import { FileDownloadServiceModule } from '../shared/file-download/file-download-service.module';
 
 @NgModule({
   declarations: [DownloadfileComponent],
+  exports: [DownloadfileComponent],
   imports: [
     CommonModule,
-    DownloadRoutingModule,
     FileDownloadServiceModule,
   ],
 })
