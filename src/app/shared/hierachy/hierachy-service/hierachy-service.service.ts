be987e8951837ca5f68730b25482cd2d4ea776da
Applemelon <32298607+Applemelon@users.noreply.github.com>
2019-05-24 10:23:02 +0200

    This needs an entire rework of the way items are handled

--- src/app/shared/hierachy/hierachy-service/hierachy-service.service.ts
+++ src/app/shared/hierachy/hierachy-service/hierachy-service.service.ts
@@ -5,8 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { HierachyServiceModule } from './hierachy-service.module';
 import { Item } from 'src/app/entities/item';
-import { map, tap } from 'rxjs/operators';
-import {promise} from 'selenium-webdriver';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: HierachyServiceModule
@@ -16,27 +15,17 @@ export class HierachyServiceService {
   private readonly GET_PATH_FUNCTION_URL = this.FUNCTIONS_URL + "getPathItems"
   private readonly ADD_ITEM_FUNCTION_URL = this.FUNCTIONS_URL + "addItem"
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'path': '',
-      'authorization': 'my-auth-token',
-    })
-  };
-
   constructor(private http: HttpClient, private authService: AuthenticationService) { }
 
-  async getChildItemsFromFirebaseFunction(path: Item[]): Observable<Item[]> {
-    await this.getHttpOptions();
-    console.log(this.httpOptions.headers);
-    this.httpOptions.headers = this.httpOptions.headers.set('path', this.generateStoreUri(path));
-    return this.http.get(this.GET_PATH_FUNCTION_URL, this.httpOptions) as Observable<Item[]>;
-  }
-
-  getHttpOptions() {
-    this.authService.getToken().then(token => {
-      console.log(token);
-      this.httpOptions.headers = this.httpOptions.headers.set('authorization', 'Bearer ' + token);
-    });
+  getChildItemsFromFirebaseFunction(path: Item[]): Observable<Item[]> {
+    const headers = {
+      headers: new HttpHeaders({
+        'path': this.generateStoreUri(path),
+        'authorization': ''
+      })
+    };
+    console.log(headers.headers.get('authorization'));
+    return this.http.get(this.GET_PATH_FUNCTION_URL, headers) as Observable<Item[]>;
   }
 
   generateStoreUri(path: Item[]): string {
@@ -53,7 +42,7 @@ export class HierachyServiceService {
 
     const options = {
       headers: new HttpHeaders({
-        'authorization': 'bearer ' + token
+        'authorization': 'Bearer ' + token
       })
     }
     return this.http.post(this.ADD_ITEM_FUNCTION_URL, { path: path, newItem: newItem }, options)
